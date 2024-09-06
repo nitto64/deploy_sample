b9eb854e0cf0abef542cb253526595e6a0f9df01
ゆうせい <Ikeura0604@gmail.com>
2024-09-06 17:09:52 +0900

    プリコンパイルの部分をコメントアウト

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,6 @@
 set -o errexit
 
 bundle install
-# Tailwind CSS のビルド
-yarn build:css
-bundle exec rake assets:precompile
+# bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
